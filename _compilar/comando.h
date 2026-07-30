93cf57e69762737b316670e7f10e9e05686b1703
JOSEEL <herobinekill@gmail.com>
2026-07-30 15:29:02 -0500

    Update comando.h

--- _compilar/comando.h
+++ _compilar/comando.h
@@ -1,1 +1,2 @@
 //nuitka --onefile --windows-console-mode=force --output-dir=dist --include-data-dir=static=static --include-data-dir=templates=templates --include-data-dir=datos_facturacion=datos_facturacion --windows-icon-from-ico=iconos/icono.ico --output-filename=mi_facturacion.exe inicializador.py
+//wix build Package.wxs -o MySysFact.msi
